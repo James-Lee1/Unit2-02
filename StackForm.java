4e5dbca5e1ffc3940e0f90e61babf3951a4a24dc
James Sanii <james@ip-172-31-8-86.ca-central-1.compute.internal>
2019-03-10 02:42:46 +0000

    Updated comments

--- StackForm.java
+++ StackForm.java
@@ -1,12 +1,29 @@
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: Mar 2019
+ * Created for: ICS4U
+ * This program uses the class Stack, and pushes three numbers to it, and
+ * pops the last value in the stack
+ *
+ ****************************************************************************/
+
 class StackForm
 {
     public static void main(String[] args)
     {
+        // Instantiates stack
         Stack stack = new Stack();
+
+        // Pushes 3 numbers
         stack.push(29);
         stack.push(2);
         stack.push(10);
+
+        // Pops last number
         System.out.println(stack.pop());
+
+        // Prints out the whole stack
         System.out.println(stack.toString());
     }
 }
